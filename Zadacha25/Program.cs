24a046312b625a1ba87cbbaf90629fb70c700197
AleksandraM <musatova.aleksandra@mail.ru>
2022-09-09 22:44:04 +0300

    Задача 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите два числа через энтер");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int Degree (int c, int d)
+{
+    int i = 1;
+    int fin = c;
+    while(i<d)
+    {
+        fin = fin*c;
+        i++;
+    }
+    return fin;
+}
+
+Console.WriteLine($"{a}, {b} -> {Degree (a, b)} ");
